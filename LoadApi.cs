098cb0015eaedc99355faad7b62e057209729554
Lime <98422417+pyrotoxic11@users.noreply.github.com>
2022-06-28 01:26:54 -0400

    Add files via upload

--- LoadApi.cs
+++ LoadApi.cs
@@ -17,7 +17,7 @@ namespace RoseClient.SDK.Load
         public static List<LoadButton> Allbutton = new List<LoadButton>();
         public static GameObject Button;
         public static string APINAME = "-LimesApi";
-        public LoadButton(Vector3 Position, Vector3 Scale, string ButtonText, string ObjName, Action action)
+        public LoadButton(Vector3 Position, Vector3 Scale, string ButtonText, string ObjName, Action action, Color LabelColor, Color ButtonColor, Sprite Buttonimage = null)
         {
             Buttonbase = UnityEngine.Object.Instantiate(LoadButtonExtensions.LoadButtonTemplate(), LoadButtonExtensions.LoadButtonTemplate().transform.parent);
             Buttonbase.name = ObjName + APINAME;
@@ -25,8 +25,11 @@ namespace RoseClient.SDK.Load
             Buttonbase.GetComponent<RectTransform>().localRotation = Quaternion.identity;
             Buttonbase.GetComponent<RectTransform>().localScale = Scale;
             Button = Buttonbase.transform.Find("GoButton").gameObject;
+            Button.GetComponent<Image>().color = ButtonColor;
+            Button.GetComponent<Image>().overrideSprite = Buttonimage;
             Label = Button.transform.Find("Text").gameObject.GetComponent<Text>();
             Label.text = ButtonText;
+            Label.color = LabelColor;
             Buttonbase.transform.Find("Decoration_Left").gameObject.SetActive(false);
             Buttonbase.transform.Find("Loading Elements").gameObject.SetActive(false);
             Buttonbase.transform.Find("Decoration_Right").gameObject.SetActive(false);
